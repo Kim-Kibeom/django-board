2ed1f959995b382fe1f0d71f170ac149d0f03690
keyBomb <gibum1995@gmail.com>
2021-12-06 15:54:10 +0900

    Add app to admin

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+admin.site.register(User)
+
+# class UserAdmin(admin.ModelAdmin):
+#     list_display = ('user_id', 'email', 'password', 'created_at', 'updated_at')
+
+# admin.site.register(User, UserAdmin)    
