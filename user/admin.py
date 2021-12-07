52d40627991d1e97de2ce1c2804bd6651f5aaa42
keyBomb <gibum1995@gmail.com>
2021-12-07 19:00:56 +0900

    Add displaying field

--- user/admin.py
+++ user/admin.py
@@ -2,9 +2,7 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'email', 'password', 'created_at', 'updated_at')
 
-# class UserAdmin(admin.ModelAdmin):
-#     list_display = ('user_id', 'email', 'password', 'created_at', 'updated_at')
-
-# admin.site.register(User, UserAdmin)    
+admin.site.register(User, UserAdmin)    
