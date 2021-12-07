1634414fc02c9f7eb4cb3c83a995771cf010096f
keyBomb <gibum1995@gmail.com>
2021-12-07 18:59:51 +0900

    Fix: fix register method

--- user/views.py
+++ user/views.py
@@ -1,4 +1,35 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.http import HttpResponseRedirect
+from django.contrib.auth.hashers import make_password
+
+from .models import User
 
 def register(request):
-    return render(request, 'user/register.html')
+    if request.method == 'GET':
+        # 로그인 진행
+        return render(request, 'user/register.html')
+    elif request.method == 'POST':
+        # 회원가입 진행
+        user_id = request.POST['userid']
+        email = request.POST['email']
+        password = request.POST['password']
+        re_password = request.POST['re_password']
+
+        # 비밀번호가 일치하지 않으면 
+        # 에러문구 출력 후 다시 register로
+        if password != re_password:
+            data = {'error': '비밀번호가 일치하지 않습니다.'}
+            return render(request, 'user/register.html', data)
+
+        user = User(
+            user_id = user_id,
+            email = email,
+            password = make_password(password),
+        )
+        user.save()
+
+        return HttpResponseRedirect(reverse('user:login'))
+
+def login(request):
+    return render(request, 'user/login.html')
