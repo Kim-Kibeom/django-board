94fd0e7f9aca8016d1bfab169895ef67c012c6ab
keyBomb <gibum1995@gmail.com>
2021-12-09 17:14:20 +0900

    Add user/login url and view

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
 
 from .models import User
 
@@ -16,6 +16,17 @@ def register(request):
         password = request.POST['password']
         re_password = request.POST['re_password']
 
+        # 이미 있는 id라면 다시 입력해야 함
+        try: 
+            find = User.objects.filter(user_id=user_id)        
+        except User.DoesNotExist:
+            data = {'error': "이미 존재하는 아이디입니다."}
+            return render(request, 'user/register.html', data)
+        # else:
+            # if password != find.password:
+            #     return render(request, 'user/login.html')
+            # else:
+            #     return HttpResponseRedirect('/blog')
         # 비밀번호가 일치하지 않으면 
         # 에러문구 출력 후 다시 register로
         if password != re_password:
@@ -32,4 +43,18 @@ def register(request):
         return HttpResponseRedirect(reverse('user:login'))
 
 def login(request):
-    return render(request, 'user/login.html')
+    if request.method == 'GET':
+        return render(request, 'user/login.html')
+    elif request.method == 'POST':
+        userid = request.POST['username']
+        password = request.POST['password']
+
+        try: 
+            find = User.objects.get(user_id=userid)        
+        except (User.DoesNotExist):
+            return render(request, 'user/login.html')
+        else:
+            if password != find.password:
+                return render(request, 'user/login.html')
+            else:
+                return HttpResponseRedirect('/blog')
